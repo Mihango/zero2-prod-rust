2954b7275e1b211fbb284911b9c6c459d0c52583
Mihango <mihangokennedy@gmail.com>
2022-03-22 18:46:09 +0300

    added a scoped health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(web::scope("/api/v1")
+                .route("/health_check", web::get().to(health_check))
+            )
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
