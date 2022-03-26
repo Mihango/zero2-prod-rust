5f292ccf6b47c4b93dc24107c7584aa6a797f1f2
Mihango <mihangokennedy@gmail.com>
2022-03-26 18:15:59 +0300

    added tcp listener to the server to allow rando port generation for testing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use std::net::TcpListener;
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(web::scope("/api/v1")
-                .route("/health_check", web::get().to(health_check))
-            )
-    })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+    run(TcpListener::bind("127.0.0.0:8080")?)?.await
 }
