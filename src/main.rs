6d66a0e461208bfa066457fa1c308fd6f3cd396a
Mihango <mihangokennedy@gmail.com>
2022-05-01 17:40:37 +0300

    added form url encoded data extractor with serde

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use crate::lib::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0")
+    let listener = TcpListener::bind("127.0.0.1:8000")
         .expect("Failed to bind to port");
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -62,7 +62,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 
         // Assert
         assert_eq!(400, response.status().as_u16(),
-                   "The API did fail with 400 Bad Request when the payload was {}", error_message);
+                   "The API did not fail with 400 Bad Request when the payload was {}", error_message);
     }
 }
 
